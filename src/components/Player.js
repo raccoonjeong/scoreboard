81c9510b6f9d497895de7acad70f80f63703e68c
kosmo_11 <rana3782@gmail.com>
2019-06-29 10:17:01 +0900

    shouldComponent 라이프사이클 메서드를 활용해서 reconcilation을 결정

--- src/components/Player.js
+++ src/components/Player.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import {Counter} from "./Counter";
 
-export class Player extends React.PureComponent {
+export class Player extends React.Component {
   render() {// 퓨어컴포넌트는 반드시 render함수가 있어야하고 엘리먼트를 리턴해야함. 
     console.log(this.props.name, 'rendered'); // 클래스 컴포넌트로 바꾸었기때문에 항상 new로 생성이 된다. this를 붙여야함
     return (
@@ -13,5 +13,11 @@ export class Player extends React.PureComponent {
         <Counter score={this.props.score} id={this.props.id} changeScore={this.props.changeScore}/>
       </div>
     )
+  }
+  componentWillReceiveProps(nextProps, nextContext) {
+  }
+  shouldComponentUpdate(nextProps, nextState, nextContext) { // 꼭 Boolean형태로 리턴해야함.
+    console.log(nextProps, nextState);
+    return nextProps.score !== this.props.score; // 변경될때만 predicate 일어나도록..
   }
 }
