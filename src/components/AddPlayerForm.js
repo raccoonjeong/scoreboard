8d1d27dd8c17dab274df47a7ee27ab4e13675027
kosmo_11 <rana3782@gmail.com>
2019-06-29 10:44:47 +0900

    controlled 컴포넌트를 uncontrolled 컴포넌트로 변환(성능개선)

--- src/components/AddPlayerForm.js
+++ src/components/AddPlayerForm.js
@@ -1,26 +1,19 @@
 import React from 'react';
 
 export class AddPlayerForm extends React.Component {
-  state = {
-    value: ''
-  };
-
-  handleChange = (e) => {
-    console.log(e);
-    this.setState({value: e.target.value})
-  };
+  textInput = React.createRef();
 
   handleSubmit = (e) => {
     // 기본이벤트(페이지 재로딩) 막기
     e.preventDefault();
-    this.props.addPlayer(this.state.value);
+    this.props.addPlayer(this.textInput.current.value); // 자바스크립트 DOM API
   };
 
   render() {
     return (
       <form className="form" onSubmit={this.handleSubmit}>
         <input className="input" type="text" placeholder="enter a player's name"
-        value={this.state.value} onChange={this.handleChange}></input>
+               ref={this.textInput}></input>
         <input className="input" type="submit" value="Add Player"></input>
       </form>
     );
