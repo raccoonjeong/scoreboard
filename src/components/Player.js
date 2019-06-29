e8473888842559911c0a79d98c7d15425b7e2f8f
kosmo_11 <rana3782@gmail.com>
2019-06-29 10:21:51 +0900

    Player 컴포넌트에 객체 해체 할당 적용

--- src/components/Player.js
+++ src/components/Player.js
@@ -4,13 +4,15 @@ import {Counter} from "./Counter";
 export class Player extends React.Component {
   render() {// 퓨어컴포넌트는 반드시 render함수가 있어야하고 엘리먼트를 리턴해야함. 
     console.log(this.props.name, 'rendered'); // 클래스 컴포넌트로 바꾸었기때문에 항상 new로 생성이 된다. this를 붙여야함
+    const {removePlayer, id, name, score, changeScore} = this.props;
+
     return (
       <div className="player">
     <span className="player-name">
-      <button className="remove-player" onClick={()=>this.props.removePlayer(this.props.id)}>X</button>
-      {this.props.name}
+      <button className="remove-player" onClick={()=>removePlayer(this.props.id)}>X</button>
+      {name}
     </span>
-        <Counter score={this.props.score} id={this.props.id} changeScore={this.props.changeScore}/>
+        <Counter score={score} id={id} changeScore={changeScore}/>
       </div>
     )
   }
