110397d8cf148337e984ab9cc5585bef33fb98a6
kosmo_11 <rana3782@gmail.com>
2019-06-29 10:04:09 +0900

    Pure Component 이용해서 원하는 엘리먼트만 렌더링하기

--- src/components/Player.js
+++ src/components/Player.js
@@ -1,12 +1,17 @@
 import React from 'react';
 import {Counter} from "./Counter";
 
-export const Player = (props) => (
-  <div className="player">
+export class Player extends React.PureComponent {
+  render() {// 퓨어컴포넌트는 반드시 render함수가 있어야하고 엘리먼트를 리턴해야함. 
+    console.log(this.props.name, 'rendered'); // 클래스 컴포넌트로 바꾸었기때문에 항상 new로 생성이 된다. this를 붙여야함
+    return (
+      <div className="player">
     <span className="player-name">
-      <button className="remove-player" onClick={()=>props.removePlayer(props.id)}>X</button>
-      {props.name}
+      <button className="remove-player" onClick={()=>this.props.removePlayer(this.props.id)}>X</button>
+      {this.props.name}
     </span>
-    <Counter score={props.score} id={props.id} changeScore={props.changeScore}/>
-  </div>
-);
+        <Counter score={this.props.score} id={this.props.id} changeScore={this.props.changeScore}/>
+      </div>
+    )
+  }
+}
