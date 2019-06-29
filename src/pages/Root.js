ad7e135d4097c3c30ddfd4dca0f5d8e5cf2df0c0
kosmo_11 <rana3782@gmail.com>
2019-06-29 18:22:30 +0900

    메뉴바 만들기

--- src/pages/Root.js
+++ src/pages/Root.js
@@ -3,13 +3,14 @@ import {BrowserRouter, Route, Switch} from "react-router-dom";
 import {Home} from "./Home";
 import {Scoreboard} from "./Scoreboard";
 import {Heroes} from "./Heroes";
+import {Menu} from "./Menu";
 
 export class Root extends React.Component {
 
   render() {
     return (
       <BrowserRouter>
-        <p>공통메뉴</p>
+        <Menu></Menu>
         { /*네스티드 라우팅 영역*/}
         <Switch>
           <Route exact path="/" component={Home}></Route>
