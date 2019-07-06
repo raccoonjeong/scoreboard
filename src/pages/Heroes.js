72110c4f6fb7235c5d0475be50a83b5483de97a9
kosmo_11 <rana3782@gmail.com>
2019-07-06 12:23:14 +0900

    목록 가져오기

--- src/pages/Heroes.js
+++ src/pages/Heroes.js
@@ -1,12 +1,36 @@
 import React from 'react';
+import axios from 'axios';
 
 export class Heroes extends React.Component {
 
+  state = {
+    heroes: [],
+
+  }
   render() {
     return (
-      <div>
-        Heroes works.
-      </div>
-    );
+      <ul>
+        {this.state.heroes.map(hero => (
+          <li key="{hero.id}">
+            <img src={hero.photo} alt={hero.name} />
+            <span>{hero.name}</span>
+          </li>
+          ))}
+      </ul>
+      );
   }
+  componentDidMount() {
+    this.getHeroes();
+    console.log(this.state);
+  };
+
+  async getHeroes() {
+    const {data} = await axios.get('http://ec2-15-164-134-124.ap-northeast-2.compute.amazonaws.com:8000/api/user/heroes');
+    console.log(data);
+    this.setState({heroes: data.data})
+  }
+
+
+
+
 }
